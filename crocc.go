aee2eadbb85eb1a76faf5cad58b6c3bc92ae34e3
Nicolas Paul <n@nc0.fr>
2023-04-25 15:34:51 +0200

    Add CI

--- crocc.go
+++ crocc.go
@@ -1,2 +1,6 @@
+// Copyright (c) 2023 Nicolas Paul All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package crocc is a simple Markdown-based static site generator.
 package crocc
---
+++ tools/addlicense.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Copyright (c) 2023 Nicolas Paul All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+addlicense -c="Nicolas Paul" -l=bsd -v -y=2023 .
