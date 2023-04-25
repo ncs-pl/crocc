12d086bf4a0dc9422ccbb3ef7b38625e4cc98d89
Nicolas Paul <n@nc0.fr>
2023-04-25 17:14:39 +0200

    Add default flags

--- crocc.go
+++ crocc.go
@@ -3,4 +3,48 @@
 // license that can be found in the LICENSE file.
 
 // Package crocc is a simple Markdown-based static site generator.
-package crocc
+package main /* import "go.nc0.fr/crocc" */
+
+import (
+	"flag"
+	"log"
+)
+
+var (
+	outputdir      = flag.String("out", "dst", "output directory")
+	url            = flag.String("url", "http://localhost", "site URL")
+	sitemap        = flag.Bool("sitemap", false, "generate sitemap.xml")
+	generateHidden = flag.Bool("hidden", false, "generate hidden pages")
+	verbose        = flag.Bool("v", false, "verbose output")
+	version        = flag.Bool("version", false, "print version and exit")
+)
+
+const usage string = `crocc is a simple Markdown-based static site generator.
+It is designed to be simple and fast, and to be used with a static web server.
+
+Author: Nicolas Paul <n@nc0.fr> (https://nc0.fr)
+License: BSD 3-Clause
+Repository: https://github.com/n1c00o/crocc
+
+Usage:
+	crocc [options] [input directory]
+	
+Options:`
+
+const Version string = "1.0.0"
+
+func init() {
+	flag.Usage = func() {
+		log.Println(usage)
+		flag.PrintDefaults()
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if *version {
+		log.Printf("crocc v%s\n", Version)
+		return
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module go.nc0.fr/crocc
 
 go 1.20
 
-require github.com/gomarkdown/markdown v0.0.0-20230322041520-c84983bdbf2a // indirect
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/adrg/frontmatter v0.2.0 // indirect
+	github.com/gomarkdown/markdown v0.0.0-20230322041520-c84983bdbf2a // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
